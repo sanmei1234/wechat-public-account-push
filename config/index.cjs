2eecc6e1a60b05d8d057ddf34324adcc08ef549d
sanmei1234 <60925734+sanmei1234@users.noreply.github.com>
2025-05-11 22:43:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oa2q67Wz0Vz8J_EILJ7FQ7e7I4Ec',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'YcnsS4Ya4HuxkByhBTW_yDTRSgqh3l3rwFG3H_YY0D0',
+      useTemplateId: '7cji_oeW4HQ1Qczwf6JSgesdWt1LreQcD-kmPvbxGQw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-07',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'YcnsS4Ya4HuxkByhBTW_yDTRSgqh3l3rwFG3H_YY0D0',
+  CALLBACK_TEMPLATE_ID: '7cji_oeW4HQ1Qczwf6JSgesdWt1LreQcD-kmPvbxGQw',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oa2q67cP-PdfaoGIiBanP6lq17Zc',
+      id: 'oa2q67Wz0Vz8J_EILJ7FQ7e7I4Ec',
     }
   ],
 
