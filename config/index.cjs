a43d52deb5ad64af06cbcb95a0062384bdcfcdda
sanmei1234 <60925734+sanmei1234@users.noreply.github.com>
2025-05-19 20:45:23 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oa2q67cP-PdfaoGIiBanP6lq17Zc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'de49R8RrXymn1fcOCnSfPv4JO-LBJ4NWbeUxc_QxHWs',
+      useTemplateId: 'pK6LucqvKyjZx_448Osw_xKhAs8nXUSMlDSntvOe7fg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-07',
       festivals: [
